53a320b588349fbb6f4d3fe5c274a4845b32f738
MiguelOntiveros <MiguelOntiveros@users.noreply.github.com>
2021-01-21 10:05:22 -0600

    se agrega contrato a los archivos .ts
    se agrega contrato a los archivos .ts

--- src/app/componentes/imagenes-d-p-poder-legal/imagenes-d-p-poder-legal.component.ts
+++ src/app/componentes/imagenes-d-p-poder-legal/imagenes-d-p-poder-legal.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImagenesDPPoderLegalComponent implements OnInit {
 
+  contratoAlmacenado = null;
+
   constructor() { }
 
   ngOnInit(): void {
+  // obtiene el storage llamado contrato
+  var contratoOriginal = localStorage.getItem('contrato');
+  this.contratoAlmacenado = JSON.parse(contratoOriginal);
+
+  const area = this.contratoAlmacenado['area'];
+  const tipo = this.contratoAlmacenado['tipo'];
+  const categoria = this.contratoAlmacenado['categoria'];
+  const folio = this.contratoAlmacenado['folio'];
+  const revision = this.contratoAlmacenado['revision'];
+  console.log(this.contratoAlmacenado);
   }
 
 }
